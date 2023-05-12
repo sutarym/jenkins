7fbbfa2f441281bb4da55de7d324026123719741
sutarym <123245418+sutarym@users.noreply.github.com>
2023-05-12 17:28:38 +0530

    Create main.tf

---
+++ main.tf
@@ -0,0 +1,23 @@
+provider "aws" {
+  region = var.aws_region
+}
+
+resource "aws_vpc" "main" {
+  cidr_block = "172.16.0.0/16"
+  instance_tenancy = "default"
+  tags = {
+    Name = "main"
+  }
+}
+
+
+
+resource "aws_instance" "myFirstInstance" {
+  ami           = var.ami_id
+  key_name = var.key_name
+  instance_type = var.instance_type
+  vpc_security_group_ids = [aws_security_group.jenkins-sg-2022.id]
+  tags= {
+    Name = var.tag_name
+  }
+}
